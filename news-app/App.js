b6a15ece130e8b8aeb4fd68956310fa8bed78b39
Yan Skvarada <yan@MacBook-Air-Yan.local>
2023-06-26 01:44:44 +0300

    add

--- news-app/App.js
+++ news-app/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native'
+// import styled from "styled-components/native";
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.testText}>127317</Text>
       <Text style={styles.testText}>127317</Text>
-      <Text style={styles.testText}>127317</Text>
       <StatusBar style="auto" />
-      <Title/>
+      {/* <Title/> */}
     </View>
   );
 }
